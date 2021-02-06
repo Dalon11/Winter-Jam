d19c16c525136999231a21b3cbfadb49c77fd4a9
Dalon11 <68819719+Dalon11@users.noreply.github.com>
2021-02-06 21:57:47 +0300

    Initial commit

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform target;
+    public float posX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0f, 0f, -30f) + new Vector3(target.position.x, target.position.y, 0f);
+        //transform.LookAt(new Vector3(target.position.x, posX, -30f));
+        
+    }
+}
